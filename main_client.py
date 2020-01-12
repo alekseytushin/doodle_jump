3730d9a9aa154a78cd08a9f6588bfbae1a1d2ca5
alekseytushin <47826650+alekseytushin@users.noreply.github.com>
2020-01-12 15:46:13 +0500

    Class connection

--- main_client.py
+++ main_client.py
@@ -1,1 +1,45 @@
-print('Hello world!')
+import pygame
+from pygame.locals import *
+import ast
+import os
+import requests
+import json
+
+
+class Connect:
+    def __init__(self):
+        self.url = "http://tushin.ru:4000/jsonrpc"
+        self.headers = {'content-type': 'application/json'}
+
+    def number_of_people(self):
+        payload = {
+            "method": "number",
+            "params": [],
+            "jsonrpc": "2.0",
+            "id": 0,
+        }
+        response = requests.post(
+            self.url, data=json.dumps(payload), headers=self.headers).json()
+        return response
+
+    def update_coord_of_people(self):
+        payload = {
+            "method": "get_info",
+            "params": [],
+            "jsonrpc": "2.0",
+            "id": 0,
+        }
+        response = requests.post(
+            self.url, data=json.dumps(payload), headers=self.headers).json()
+        return response
+
+    def set_coord(self, x, y, direction):
+        payload = {
+            "method": "set_coord",
+            "params": [x, y, direction],
+            "jsonrpc": "2.0",
+            "id": 0,
+        }
+        response = requests.post(
+            self.url, data=json.dumps(payload), headers=self.headers).json()
+        return response
