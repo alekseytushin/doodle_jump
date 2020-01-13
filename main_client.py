0e356bb2e0c5fa9bd79e7b04ccb3564e9a9b3109
alekseytushin <47826650+alekseytushin@users.noreply.github.com>
2020-01-13 22:46:34 +0500

    Completely remade the logic of the server

--- main_client.py
+++ main_client.py
@@ -2,18 +2,15 @@ import pygame
 from pygame.locals import *
 import ast
 import os
-import requests
-import json
+import socket
 import sys
+import time
 
 
 class Connect:
     def __init__(self):
-        """
-        Класс по подключению игрока к серверу.
-        """
-        self.url = "http://tushin.ru:4000/jsonrpc"
-        self.headers = {'content-type': 'application/json'}
+        self.sock = socket.socket()
+        self.sock.connect(("212.33.255.45", 4000))
 
     def add_user(self):
         """
@@ -21,94 +18,70 @@ class Connect:
         Возвращает False, если все места заняты.
         Возвращает user_id если на сервере есть место.
         """
-        payload = {
-            "method": "new_user",
-            "params": [],
-            "jsonrpc": "2.0",
-            "id": 0,
-        }
-        response = requests.post(
-            self.url, data=json.dumps(payload), headers=self.headers).json()
-        return response['result']
+
+        self.sock.send(b'new_user')
+        answer = self.sock.recv(512).decode("utf-8")
+        if answer.isdigit():
+            answer = int(answer)
+        return answer
 
     def get_unitlist(self):
         """
         Фукция, которая возвращает список игроков с их координатами и направлением
         """
-        payload = {
-            "method": "number",
-            "params": [],
-            "jsonrpc": "2.0",
-            "id": 0,
-        }
-        response = requests.post(
-            self.url, data=json.dumps(payload), headers=self.headers).json()
-        return response['result']
+        self.sock.send(b'number')
+        answer = eval(self.sock.recv(512).decode("utf-8"))
+        return answer
 
-    def set_coord(self, x, y, direction, num_id):
+    def set_coord(self, x, y, direction, num_id, sprite_num):
         """
         Функция, для записывания координат игрока на сервер.
         На вход получает координаты(x, y) и направление движения человека.
         Возвращает True, если можно переместиться или False если клетка занята
         """
-        payload = {
-            "method": "set_coord",
-            "params": [x, y, direction, num_id],
-            "jsonrpc": "2.0",
-            "id": 0,
-        }
-        response = requests.post(
-            self.url, data=json.dumps(payload), headers=self.headers).json()
-        return response['result']
+        string = ' '.join([str(x), str(y), str(direction), str(num_id), str(sprite_num)])
+        self.sock.send(b'set_coord')
+        self.sock.send(str(len(string)).encode("utf-8"))
+        self.sock.send(string.encode("utf-8"))
 
     def disconnect(self, user_id):
         """
         Функция для выхода из игры.
         """
-        payload = {
-            "method": "disconnect",
-            "params": [user_id],
-            "jsonrpc": "2.0",
-            "id": 0,
-        }
-        response = requests.post(
-            self.url, data=json.dumps(payload), headers=self.headers).json()
-        return
-
-
-class AnimatedSprite(pygame.sprite.Sprite):
-    def __init__(self, sheet, columns, rows, x, y):
-        super().__init__(all_sprites)
-        self.frames = []
-        self.cut_sheet(sheet, columns, rows)
-        self.cur_frame = 0
-        self.image = self.frames[self.cur_frame]
-        self.rect = self.rect.move(x, y)
-
-    def cut_sheet(self, sheet, columns, rows):
-        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
-                                sheet.get_height() // rows)
-        for j in range(rows):
-            for i in range(columns):
-                frame_location = (self.rect.w * i, self.rect.h * j)
-                self.frames.append(sheet.subsurface(pygame.Rect(
-                    frame_location, self.rect.size)))
-
-    def update(self):
-        self.cur_frame = (self.cur_frame + 1) % len(self.frames)
-        self.image = self.frames[self.cur_frame]
+        self.sock.send(b'disconnect')
+        self.sock.send(str(user_id).encode("utf-8"))
+        self.sock.close()
 
 
 class Unit:
-    def __init__(self, x, y, direction, num, obj, img=None):
-        image = [['first_user_left.png', 'first_user_right.png']]
+    def __init__(self, x, y, direction, num, obj, img=None, sprite_num=None):
         self.x = x
         self.y = y
         self.obj = obj
+        walkRight = [pygame.image.load('game_files/pygame_right_1.png'),
+                     pygame.image.load('game_files/pygame_right_2.png'),
+                     pygame.image.load('game_files/pygame_right_3.png'),
+                     pygame.image.load('game_files/pygame_right_4.png'),
+                     pygame.image.load('game_files/pygame_right_5.png'),
+                     pygame.image.load('game_files/pygame_right_6.png')]
+
+        walkLeft = [pygame.image.load('game_files/pygame_left_1.png'),
+                    pygame.image.load('game_files/pygame_left_2.png'),
+                    pygame.image.load('game_files/pygame_left_3.png'),
+                    pygame.image.load('game_files/pygame_left_4.png'),
+                    pygame.image.load('game_files/pygame_left_5.png'),
+                    pygame.image.load('game_files/pygame_left_6.png')]
+
+        stand = pygame.image.load('game_files/pygame_idle.png')
         if obj == 'player':
             self.imgheight = 400
             self.imgwidth = 500
-            self.sprite = pygame.image.load(image[0][direction])
+            if direction == 0:
+                self.sprite = walkLeft[sprite_num]
+            elif direction == 1:
+                self.sprite = walkRight[sprite_num]
+            elif direction == 2:
+                self.sprite = stand
         else:
             self.sprite = img
             self.imgwidth = img.get_width()
@@ -128,17 +101,18 @@ class Hero:
         self.y = y
         self.num_id = num_id
         self.direction = direction
-        self.imgheight = 400
-        self.imgwidth = 500
+        self.imgheight = 71
+        self.imgwidth = 60
         self.speed = 10
+        self.sprite_num = 0
 
     def get_collision_list(self, unit):
         if unit.obj == 'player':
             pass
         elif unit.obj == 'ground':
             if self.x + self.imgwidth >= unit.x >= self.x or unit.x + unit.imgwidth >= self.x >= unit.x:
-                if self.y + self.imgheight >= unit.y + 20 and self.y + self.imgheight < unit.y + unit.imgheight:
-                    self.y = unit.y - 20 - self.imgheight
+                if unit.y + 10 <= self.y + self.imgheight < unit.y + unit.imgheight:
+                    self.y = unit.y - self.imgheight
                     self.onGround = True
 
     def move(self, forces, keys, connection, UNITLIST):
@@ -147,25 +121,40 @@ class Hero:
             result_force[0] += force[0]
             result_force[1] += force[1]
         result_force[1] += self.jump_force
-        self.x += result_force[0] * self.K_MOVE
-        self.y += result_force[1] * self.K_MOVE
+        self.x += result_force[0] * self.K_MOVE / 5
+        self.y += result_force[1] * self.K_MOVE / 5
 
         if keys[pygame.K_a]:
             self.x -= self.speed * self.K_MOVE
+            if self.direction == 1:
+                self.sprite_num = 0
             self.direction = 0
-        if keys[pygame.K_d]:
+            self.sprite_num += 1
+        elif keys[pygame.K_d]:
             self.x += self.speed * self.K_MOVE
+            if self.direction == 0:
+                self.sprite_num = 0
             self.direction = 1
+            self.sprite_num += 1
+        else:
+            self.direction = 2
+            self.sprite_num = 0
+        if self.sprite_num > 5:
+            self.sprite_num = 0
+
         if keys[pygame.K_SPACE]:
             if self.onGround:
                 self.onGround = False
-                self.jump_force = 500
-                self.y -= self.jump_force * self.K_MOVE
-                self.jump_force -= self.jump_force / 10
+                self.jump_force = 250
+
+        # for jump
+        if self.jump_force > 20:
+            self.y -= self.jump_force / 2
+            self.jump_force -= self.jump_force / 10
 
         for i in range(len(UNITLIST)):
             self.get_collision_list(UNITLIST[i])
-        connection.set_coord(self.x, self.y, self.direction, self.num_id)
+        connection.set_coord(self.x, self.y, self.direction, self.num_id, self.sprite_num)
 
 
 class Platform(Unit):
@@ -189,13 +178,14 @@ class Client:
         players = connection.get_unitlist()
         character = Hero(players[character_id][0], players[character_id][1], players[character_id][2], character_id)
         run = True
-        bg = pygame.image.load("bg.png")
+        bg = pygame.image.load("game_files/bg.png")
         clock = pygame.time.Clock()
         Plats = []
         num = 1
         xPL = 230
         yPL = 800
         while run:
+            clock.tick(120)
             self.UNITLIST = []
             window.blit(bg, (0, 0))
             for event in pygame.event.get():
@@ -204,17 +194,15 @@ class Client:
             players = connection.get_unitlist()
             for i in range(len(players)):
                 if len(players[i]) != 0 and (1920 + 512) > players[i][0] > -512 and (1080 + 512) > players[i][1] > -512:
-                    self.UNITLIST.append(Unit(players[i][0], players[i][1], players[i][2], i, 'player'))
-            for i in range(num):
-                Plats.append(Platform(xPL, yPL, pygame.image.load("Platform.jpg")))
-                self.UNITLIST.append(Plats[-1])
-                xPL += 5
+                    self.UNITLIST.append(Unit(players[i][0], players[i][1],
+                                              players[i][2], i, 'player', sprite_num=players[i][3]))
+            Plats.append(Platform(xPL, yPL, pygame.image.load("game_files/Platform.jpg")))
+            self.UNITLIST.append(Plats[-1])
             if pygame.key.get_pressed():
-                character.move([(0, 7 * 10)], pygame.key.get_pressed(), connection, self.UNITLIST)
+                character.move([(0, 9 * 10)], pygame.key.get_pressed(), connection, self.UNITLIST)
             for unit in self.UNITLIST:
                 unit.render(window)
             pygame.display.update()
-            clock.tick(240)
         pygame.quit()
 
 
--- main_server.py
+++ main_server.py
@@ -1,9 +1,6 @@
-from werkzeug.wrappers import Request, Response
-from werkzeug.serving import run_simple
-from jsonrpc import JSONRPCResponseManager, dispatcher
+import socket
 
 
-@dispatcher.add_method
 def new_user():
     """
     Функция, которая отвечает за обработку запросов и формирования ответа.
@@ -12,50 +9,47 @@ def new_user():
     global players
     for i in range(len(players)):
         if len(players[i]) == 0:
-            players[i] = [240 + i * 480, 0, i % 2]
+            players[i] = [240 + i * 480, 0, 2, 0]
             return i
     return False
 
 
-@dispatcher.add_method
-def number():
-    """
-    Функция, которая возвращает всю информацию о игроках.
-    """
-    global players
-    return players
-
-
-@dispatcher.add_method
 def disconnect(user_id):
     """
     Функция, которая удаляет персонажа с поля
     """
     global players
     players[user_id] = []
-    return
 
 
-@dispatcher.add_method
-def set_coord(x, y, direction, user_id):
+def set_coord(x, y, direction, user_id, sprite_num):
     """
     Функция, которая обновляет координаты и направление игрока.
     """
     global players
-    players[user_id] = (x, y, direction)
-    return True
-
-
-@Request.application
-def application(request):
-    """
-    Обработка всех входящих запросов
-    """
-    response = JSONRPCResponseManager.handle(
-        request.data, dispatcher)
-    return Response(response.json, mimetype='application/json')
+    players[user_id] = (x, y, direction, sprite_num)
 
 
 if __name__ == '__main__':
+    sock = socket.socket()
+    sock.bind(('192.168.10.5', 4000))
+    sock.listen(4)
     players = [[], [], [], []]
-    run_simple('192.168.10.5', 4000, application)
+    cou = 0
+    try:
+        conn, addr = sock.accept()
+        while True:
+            data = conn.recv(512).decode("utf-8")
+            if data == 'new_user':
+                a = new_user()
+                conn.send(str(a).encode("utf-8"))
+            elif data == 'number':
+                conn.send(str(players).encode("utf-8"))
+            elif data == 'disconnect':
+                disconnect(int(conn.recv(512).decode("utf-8")))
+            elif data == 'set_coord':
+                i = conn.recv(2).decode("utf-8")
+                text = conn.recv(int(i)).decode("utf-8").split()
+                set_coord(float(text[0]), float(text[1]), int(text[2]), int(text[3]), int(text[4]))
+    finally:
+        sock.close()
